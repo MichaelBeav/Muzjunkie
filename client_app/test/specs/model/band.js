42d4c86ca03edb71fe18939e2bbcfe557d2662ee
filipovskii_off <filipovskii.off@gmail.com>
2012-04-12 19:48:57 +0400

    clean up

--- client_app/test/specs/model/band.js
+++ client_app/test/specs/model/band.js
@@ -4,18 +4,22 @@ define([
   , 'datasource'
   ], function  (Backbone, Band, datasource) {
   describe('Band model', function  () {
+    
+    var band;
 
     function createBand() {
       return new Band();
     }
 
-    it('is a backbone model', function isAModel () {
-      var band = createBand();
+    beforeEach(function beforeTest() {
+      band = createBand();
+    });
+
+    it('is a backbone model', function isAModel() {
       expect(band).to.have.keys('cid', 'attributes', 'changed');
     });
 
-    it('gets urlRoot from datasource file', function urlRootFrmDataSrc () {
-      var band = createBand();
+    it('gets urlRoot from datasource file', function urlRootFrmDataSrc() {
       expect(band.urlRoot).to.be(datasource.bandRoot);
     });
 
