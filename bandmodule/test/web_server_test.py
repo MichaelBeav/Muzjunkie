3419a94b58006246d952bc6786b9290ef57302be
filipovskii_off <filipovskii.off@gmail.com>
2012-04-29 23:49:46 +0400

    nosetest to the rescure

--- bandmodule/test/web_server_test.py
+++ bandmodule/test/web_server_test.py
@@ -1,14 +1,19 @@
 #import json
 from bandmodule import web_server
-#from bandmodule.model import Band
+from bandmodule.model import Band
 
 from webtest import TestApp
-#from nose.tools import *
+from nose.tools import assert_true
 
 class TestBandController(object):
 
     def setup(self):
+        self.band_catalog = dict()
+        web_server.BandController.catalog = self.band_catalog
         self.app = TestApp(web_server.app.wsgifunc(*[]))
 
-    def smth_test(self):
-        print(self.app)
+    def on_post_saves_band_in_catalog(self):
+        band = Band(id='1')
+        self.app.post_json(band)
+        band
+        assert_true(True)
